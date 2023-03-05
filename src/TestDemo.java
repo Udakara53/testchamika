0dde8b7545639bda3d55564ed7a902056ff0e2d2
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-05 20:39:01 +0530

    ADD PRINT MORE 2

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,5 +1,7 @@
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("okay");
+        System.out.println("okay1");
+        System.out.println("okay2");
     }
 }
